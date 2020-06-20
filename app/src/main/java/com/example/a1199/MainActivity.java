5fd5a3b691af2d5727a8e21823346387da9611d8
JoSooBin <ssss6756@naver.com>
2020-06-21 01:06:24 +0900

    5차수정. 고민중

--- app/src/main/java/com/example/a1199/MainActivity.java
+++ app/src/main/java/com/example/a1199/MainActivity.java
@@ -91,23 +91,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void DClick1() { //삭제를 위한 빌드업
-                ArrayList<String> list = new ArrayList<>();//리스트 list 생성
+                //ArrayList<String> list = new ArrayList<>();//리스트 list 생성
+                File list = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures");//경로지정. //Pictures경로 하위에서 파일들 가져오기
                 List<String> fName1 = new ArrayList<>(); //리스트 fName1생성
+                List<String> fName2 = new ArrayList<>(); //리스트 fName1생성
 
                 File files1 = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures");//경로지정. //Pictures경로 하위에서 파일들 가져오기
 
-                ArrayAdapter<String> filelist1 = new ArrayAdapter<String>(this, layout.simple_list_item_1,list); //filelist1에 list리스트를 ListView에 띄울것임을 선언
+                ArrayAdapter<String> filelist1 = new ArrayAdapter<String>(this, simple_list_item_1,fName2); //filelist1에 list리스트를 ListView에 띄울것임을 선언
                 if(files1.listFiles().length>0){
                     for(File file : files1.listFiles()){
                         fName1.add(formatFileSize(file.length()));//formatFileSize 메소드 사용해서 long -> byte으로 바꿔줌 //각 파일의 용량 찾아서 리스트 fName1에 저장
+
+                        //list.add(file.length());
                     }
                 }
 
-                for(int i=0; i<fName1.size(); i++){ // fName1에서 중복 항목을 빼고 list에 넣기위한 반복문
-                    if(!list.contains(fName1.get(i))) {   // list에 포함되어있는지 아닌지 체크
-                        list.add(fName1.get(i)); // 해당 값이 없으면 넣기
+                for(int i=0; i<files1.listFiles().length; i++){ // fName1에서 중복 항목을 빼고 list에 넣기위한 반복문
+                    if(!fName2.contains(fName1.get(i))) {   // list에 포함되어있는지 아닌지 체크
+                        fName2.add(fName1.get(i)); // 해당 값이 없으면 넣기
                     }
                 }//파일 이름이 아니라 크기표기
+
 
                 fFind_ListView1.setAdapter(filelist1);//filelist1을 보내기
 
